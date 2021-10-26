829ec0bf630a1c1af85fe7a4cae4003288bf3a1b
krishtvr <krishtvr1995@gmail.com>
2021-10-26 17:51:24 +0530

    first

---
+++ src/org/add/GreensAddress.java
@@ -0,0 +1,27 @@
+package org.add;
+
+public class GreensAddress {
+	private void greensOmr() {
+		// TODO Auto-generated method stub
+System.out.println("perumbakkam");
+	}
+private void greensAdayar() {
+	// TODO Auto-generated method stub
+System.out.println("adayar");
+}
+private void greensTambaram() {
+	// TODO Auto-generated method stub
+System.out.println("tambaram");
+}
+private void greensVelacerry() {
+	// TODO Auto-generated method stub
+System.out.println("super");
+}
+public static void main(String[] args) {
+	GreensAddress CAT = new GreensAddress();
+	CAT.greensAdayar();
+	CAT.greensOmr();
+	CAT.greensTambaram();
+	CAT.greensVelacerry();
+}
+}
